17a6e8bf6fe5643191ff24ca3baa66e2050176f2
Gopal <gopalkishnabasudevan979@gmail.com>
2022-12-14 19:31:37 +0530

    start programming

---
+++ src/Control_statement/Hello.java
@@ -0,0 +1,12 @@
+package Control_statement;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to new journy ");
+		System.out.println("welcome for your won progress");
+
+	}
+
+}
---
+++ src/Control_statement/Loop_intro.java
@@ -0,0 +1,8 @@
+package Control_statement;
+
+public class Loop_intro {
+int a = 5;
+int b = 5;
+int c = a+b;
+System.out.print("hello world");
+}
